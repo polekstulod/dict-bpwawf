3c046627dcea3428a814eb3594f6256651c13dbc
James Paul Tulod <paultulod@pm.me>
2022-12-29 21:02:09 +0800

    Completed Module 2 part 1 🥳️

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+characters = [
+    {
+        "character": "Arthurius",
+        "power": "Telepathy",
+        "weapon": "Sword",
+        "description": "Really smart but grades are pretty low, which is surprising because he should be able to read "
+                       "his professors' minds during a test. Maybe he chooses not to. He's also super-friendly and "
+                       "kind."
+    },
+    {
+        "character": "Fantasia",
+        "power": "Telekinesis",
+        "weapon": "Crossbow",
+        "description": "Shy and likes to play with talking raccoons."
+    },
+    {
+        "character": "Obsidian",
+        "power": "Invisibility",
+        "weapon": "Spear",
+        "description": "No one knows for sure who Obsidian really is. This boy just popped out of nowhere one day. "
+                       "Many people suspect that he's always been around, silently staring at everyone."
+    }
+]
 
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World!</h1>'
+    return render_template('home.html', word="World", title="Home")
+
+
+@app.route('/character')
+def character():
+    return render_template('character.html', title="Characters", characters=characters)
 
 
 if __name__ == '__main__':
